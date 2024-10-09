8766bb6cebce354f36cb8051cf69b21385c1fda5
dito <info@magicomundo.co>
2024-10-09 15:58:12 -0500

    alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,7 +59,6 @@ export class AppComponent implements OnInit {
         private metaTagService: Meta
     ) {
 
-        this.titleService.setTitle(this.negocio.configuracion.NombreCliente);
 
         if (isPlatformBrowser(this.platformId)) {
 
@@ -123,77 +122,53 @@ export class AppComponent implements OnInit {
     private setMetaTags(): void {
         const { configuracionSitio, redes } = this.StoreSvc;
         // Validar que configuracionSitio esté definido antes de agregar las meta tags
-        if (configuracionSitio) {
-            // this.metaTagService.addTags([
-            //     // Meta generales
-            //     { name: 'description', content: configuracionSitio.PosicionamientoEnGoogle  },
-            //     { name: 'author', content: configuracionSitio.email  },
-            //     { name: 'address', content: configuracionSitio.address  },
-            //     { name: 'phone', content: configuracionSitio.phone  },
-            //     { name: 'hours', content: configuracionSitio.hours  },
-
-            //     // Meta para redes sociales (Open Graph)
-            //     { property: 'og:title', content: this.negocio.configuracion.NombreCliente },
-            //     { property: 'og:description', content: configuracionSitio.PosicionamientoEnGoogle  },
-            //     { property: 'og:type', content: 'website' },
-            //     { property: 'og:url', content: document.location.href },
-            //     { property: 'og:image', content: document.location.href + '/assets/configuracion/LOGO2.png' }, 
-            //     { property: 'og:email', content: configuracionSitio.email  },
-            //     { property: 'og:phone_number', content: configuracionSitio.phone  },
-            //     { property: 'og:address', content: configuracionSitio.address  },
-            //     { property: 'og:hours', content: configuracionSitio.hours  },
-
-            //     // // Meta para WhatsApp o contacto
-            //     // { name: 'contact:phone_number', content: configuracionSitio.NumeroWpp.toString()  },
-
-            //     // // Meta para Twitter
-            //     // { name: 'twitter:card', content:'summary_large_image' },
-            //     // { name: 'twitter:site', content: '@' + redes[1].url  },
-            //     // { name: 'twitter:creator', content: '@' + redes[1].url  },
-            //     // { name: 'twitter:title', content: this.negocio.configuracion.NombreCliente },
-            //     // { name: 'twitter:description', content: configuracionSitio.PosicionamientoEnGoogle  },
-            //     // { name: 'twitter:image', content: document.location.href + '/assets/configuracion/LOGO2.png' }, // Cambiar si tienes una imagen representativa
-
-            //     // // Meta para Facebook
-            //     // { property: 'fb:app_id', content: redes[1].url  },
-            //     // { property: 'fb:pages', content: redes[1].url  },
-            //     // { property: 'og:site_name', content: this.negocio.configuracion.NombreCliente },
-            //     // { property: 'og:description', content: configuracionSitio.PosicionamientoEnGoogle  },
-            //     // { property: 'og:image', content: document.location.href + '/assets/configuracion/LOGO2.png' },
-
-            //     // // Meta para instagram
-            //     // { name: 'instagram:username', content: 'magicomundo.co' },
-            //     // { name: 'og:image:width', content: '640' },
-            //     // { name: 'og:image:height', content: '640' },
-            // ]);
-
+        if (isPlatformBrowser(this.platformId) && configuracionSitio) {
             this.metaTagService.addTags([
-                { name: 'description', content: 'Demo Carro Mágico, la mejor tienda online para encontrar productos mágicos y únicos. Descubre nuestras ofertas.' },
-                { name: 'keywords', content: 'carro mágico, tienda online, ecommerce, productos mágicos, ofertas, compra online' },
-                { name: 'author', content: 'Demo Carro Mágico' },
-                { name: 'robots', content: 'index, follow' },
+                // Meta generales
+                { name: 'description', content: configuracionSitio.PosicionamientoEnGoogle  },
+                { name: 'author', content: configuracionSitio.email  },
+                { name: 'address', content: configuracionSitio.address  },
+                { name: 'phone', content: configuracionSitio.phone  },
+                { name: 'hours', content: configuracionSitio.hours  },
 
+                // Meta para redes sociales (Open Graph)
                 { property: 'og:title', content: this.negocio.configuracion.NombreCliente },
                 { property: 'og:description', content: configuracionSitio.PosicionamientoEnGoogle  },
                 { property: 'og:type', content: 'website' },
                 { property: 'og:url', content: document.location.href },
                 { property: 'og:image', content: document.location.href + '/assets/configuracion/LOGO2.png' }, 
+                { property: 'og:email', content: configuracionSitio.email  },
+                { property: 'og:phone_number', content: configuracionSitio.phone  },
+                { property: 'og:address', content: configuracionSitio.address  },
+                { property: 'og:hours', content: configuracionSitio.hours  },
 
-                // { property: 'og:title', content: 'Demo Carro Mágico - Tienda Online' },
-                // { property: 'og:description', content: 'Bienvenido a Demo Carro Mágico, tu tienda de productos mágicos y sorprendentes.' },
-                // { property: 'og:type', content: 'website' },
-                // { property: 'og:url', content: 'https://copiacarro--magico-mundo.us-central1.hosted.app/' },
-                // { property: 'og:image', content: 'https://copiacarro--magico-mundo.us-central1.hosted.app/assets/configuracion/LOGO2.png' },
+                // Meta para WhatsApp o contacto
+                { name: 'contact:phone_number', content: configuracionSitio.NumeroWpp.toString()  },
 
-                { name: 'twitter:card', content: 'summary_large_image' },
-                { name: 'twitter:title', content: 'Demo Carro Mágico - Tienda Online de Productos Mágicos' },
-                { name: 'twitter:description', content: 'Explora productos únicos y ofertas especiales en Demo Carro Mágico.' },
-                { name: 'twitter:image', content: 'https://copiacarro--magico-mundo.us-central1.hosted.app/assets/configuracion/LOGO2.png' },
+                // Meta para Twitter
+                { name: 'twitter:card', content:'summary_large_image' },
+                { name: 'twitter:site', content: '@' + redes[1].url  },
+                { name: 'twitter:creator', content: '@' + redes[1].url  },
+                { name: 'twitter:title', content: this.negocio.configuracion.NombreCliente },
+                { name: 'twitter:description', content: configuracionSitio.PosicionamientoEnGoogle  },
+                { name: 'twitter:image', content: document.location.href + '/assets/configuracion/LOGO2.png' }, // Cambiar si tienes una imagen representativa
+
+                // Meta para Facebook
+                { property: 'fb:app_id', content: redes[1].url  },
+                { property: 'fb:pages', content: redes[1].url  },
+                { property: 'og:site_name', content: this.negocio.configuracion.NombreCliente },
+                { property: 'og:description', content: configuracionSitio.PosicionamientoEnGoogle  },
+                { property: 'og:image', content: document.location.href + '/assets/configuracion/LOGO2.png' },
+
+                // Meta para instagram
+                { name: 'instagram:username', content: 'magicomundo.co' },
+                { name: 'og:image:width', content: '640' },
+                { name: 'og:image:height', content: '640' },
             ]);
 
-            alert(document.location.href)
+            this.titleService.setTitle(this.negocio.configuracion.NombreCliente);
         } else {
-            console.error('No se encontró la configuración del sitio.');
+            alert('No se encontró la configuración del sitio.');
         }
     }
 
