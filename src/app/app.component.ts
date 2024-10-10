30a012d67024df24d0d36e3c5e3e73355b4fec85
dito <info@magicomundo.co>
2024-10-10 08:45:22 -0500

    REFACTOR

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,44 @@
-import { Component, Inject, NgZone, OnInit, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, NgZone, OnInit, PLATFORM_ID, signal } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { ToastrService } from 'ngx-toastr';
 import { CartService } from './shared/services/cart.service';
 import { CompareService } from './shared/services/compare.service';
 import { WishlistService } from './shared/services/wishlist.service';
-
-
-import { CommonModule, DOCUMENT, isPlatformBrowser, ViewportScroller } from '@angular/common';
+import { DOCUMENT, isPlatformBrowser, ViewportScroller } from '@angular/common';
 import { NavigationEnd, Router, RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { filter, first } from 'rxjs/operators';
 import { CurrencyService } from './shared/services/currency.service';
-
 import { NegocioService } from './shared/services/negocio.service';
-
-// utils
 import { UtilsTexto } from '../app/shared/utils/UtilsTexto';
+import { StoreService } from './shared/services/store.service';
 import { RootComponent } from './components/root/root.component';
 import { BlocksModule } from './modules/blocks/blocks.module';
 import { HeaderModule } from './modules/header/header.module';
 import { MobileModule } from './modules/mobile/mobile.module';
+import { ShopModule } from './modules/shop/shop.module';
 import { UtilsModule } from './modules/utils/utils.module';
 import { WidgetsModule } from './modules/widgets/widgets.module';
-import { StoreService } from './shared/services/store.service';
 import { SharedModule } from './shared/shared.module';
-import { ShopModule } from './modules/shop/shop.module';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [
-        BlocksModule,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [BlocksModule,
         HeaderModule,
         MobileModule,
         ShopModule,
         SharedModule,
         WidgetsModule,
-        UtilsModule, RootComponent, RouterModule, RouterLink, RouterOutlet],
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss']
+        UtilsModule, RootComponent, RouterModule, RouterLink, RouterOutlet
+    ],
 })
 export class AppComponent implements OnInit {
-
-    private title: string;
-    private urlImage: string;
-    private description: string;
-    private urlPublic: string;
+    title = signal<string>('');         // Inicia con una cadena vacía
+    urlImage = signal<string>('');      // Inicia con una cadena vacía
+    description = signal<string>('');   // Inicia con una cadena vacía
+    urlPublic = signal<string>('');     // Inicia con una cadena vacía
 
     constructor(
         @Inject(PLATFORM_ID) private platformId: Object,
@@ -60,98 +54,94 @@ export class AppComponent implements OnInit {
         private currency: CurrencyService,
         private negocio: NegocioService,
         private titleService: Title,
-        public StoreSvc: StoreService,
+        public storeService: StoreService,
         private metaService: Meta
     ) {
-
-
         if (isPlatformBrowser(this.platformId)) {
+            this.initializeTrackingScript();
+            this.setupPreloader();
+        }
+    }
 
-            eval(this.StoreSvc?.configuracionSitio?.scriptRastreo);
+    ngOnInit(): void {
+        if (isPlatformBrowser(this.platformId)) {
+            this.initializeMetaInfo();
+        }
 
+        this.router.events.pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe(() => this.scroller.scrollToPosition([0, 0]));
 
-            this.zone.runOutsideAngular(() => {
-                this.router.events.pipe(filter(event => event instanceof NavigationEnd), first()).subscribe(() => {
-                    const preloader = document.querySelector('.site-preloader');
+        this.subscribeToCartEvents();
+        this.subscribeToCompareEvents();
+        this.subscribeToWishlistEvents();
+    }
 
+    private initializeTrackingScript(): void {
+        if (this.storeService?.configuracionSitio?.scriptRastreo) {
+            // Se recomienda evitar eval(); intenta cargar el script de forma segura.
+            // Por ejemplo, añadiendo un script al DOM dinámicamente
+            const script = this.document.createElement('script');
+            script.text = this.storeService.configuracionSitio.scriptRastreo;
+            this.document.head.appendChild(script);
+        }
+    }
+
+    private setupPreloader(): void {
+        this.zone.runOutsideAngular(() => {
+            this.router.events.pipe(filter(event => event instanceof NavigationEnd), first()).subscribe(() => {
+                const preloader = this.document.querySelector('.site-preloader');
+                if (preloader) {
                     preloader.addEventListener('transitionend', (event: TransitionEvent) => {
                         if (event.propertyName === 'opacity') {
                             preloader.remove();
                         }
                     });
                     preloader.classList.add('site-preloader__fade');
-                });
+                }
             });
-        }
-
-
-
-
-
-    }
-
-    TitleCase(texto) {
-        texto = texto.toLowerCase().replace(/\b[a-z]/g, txt => {
-            return txt.toUpperCase();
         });
-
-        return texto;
     }
 
-    ngOnInit(): void {
-        const { configuracionSitio } = this.StoreSvc;
-        this.title = this.negocio.configuracion.NombreCliente;
-        // this. urlImage= "https://copiacarro--magico-mundo.us-central1.hosted.app/assets/configuracion/LOGO2.png";
-        this.titleService.setTitle(this.title ?? '');
-        this.description = configuracionSitio?.PosicionamientoEnGoogle ?? 'falto';
-        this.urlImage = `${this.document.baseURI}/asset/configuracion/LOGO2.png`;
-        this.urlPublic = this.document.baseURI ?? '';
+    private initializeMetaInfo(): void {
+        const { configuracionSitio } = this.storeService;
+        this.title.set(this.negocio.configuracion.NombreCliente || ''); // fallback en caso de que sea undefined
+        this.description.set(configuracionSitio?.PosicionamientoEnGoogle || 'Descripción predeterminada');
+        this.urlImage.set(`${this.document.baseURI}/asset/configuracion/LOGO2.png`);
+        this.urlPublic.set(this.document.baseURI || '');
 
+        this.titleService.setTitle(this.title() || '');
         this.setMetaTags();
-        // properties of the CurrencyFormatOptions interface fully complies
-        // with the arguments of the built-in pipe "currency"
-        // https://angular.io/api/common/CurrencyPipe
-        this.currency.options = {
-            code: 'COP',
-            display: 'code',
-            digitsInfo: '1.0-2',
-            // locale: 'en-US'
-        };
+    }
 
-        this.router.events.subscribe((event) => {
-            if ((event instanceof NavigationEnd)) {
-                this.scroller.scrollToPosition([0, 0]);
-            }
-        });
+    private subscribeToCartEvents(): void {
         this.cart.onAdding$.subscribe(product => {
-            this.toastr.success(`Producto "${this.utils.TitleCase(product.name)}" Agregado al Carrito!`);
+            this.toastr.success(`Producto "${this.utils.TitleCase(product.name)}" agregado al carrito!`);
         });
+    }
+
+    private subscribeToCompareEvents(): void {
         this.compare.onAdding$.subscribe(product => {
-            this.toastr.success(`Producto "${this.utils.TitleCase(product.name)}" Agregado para Comparar!`);
+            this.toastr.success(`Producto "${this.utils.TitleCase(product.name)}" agregado para comparar!`);
         });
+    }
+
+    private subscribeToWishlistEvents(): void {
         this.wishlist.onAdding$.subscribe(product => {
-            this.toastr.success(`Producto "${this.TitleCase(product.name)}" Agregado a la Lista de Deseos!`);
+            this.toastr.success(`Producto "${this.utils.TitleCase(product.name)}" agregado a la lista de deseos!`);
         });
-
-
     }
 
     private setMetaTags(): void {
         this.metaService.addTags([
-            { name: 'description', content: this.description },
-            { name: 'og:title', content: this.title },
-            { name: 'og:description', content: this.description },
-            { name: 'og:image', content: this.urlImage },
-            { name: 'og:url', content: this.urlPublic },
+            { name: 'description', content: this.description() },
+            { name: 'og:title', content: this.title() },
+            { name: 'og:description', content: this.description() },
+            { name: 'og:image', content: this.urlImage() },
+            { name: 'og:url', content: this.urlPublic() },
             { name: 'twitter:card', content: 'summary_large_image' },
-            { name: 'twitter:title', content: this.title },
-            { name: 'twitter:description', content: this.description },
-            { name: 'twitter:image', content: this.urlImage },
+            { name: 'twitter:title', content: this.title() },
+            { name: 'twitter:description', content: this.description() },
+            { name: 'twitter:image', content: this.urlImage() },
         ]);
-        
-        alert(this.description)
     }
-
-
 }
-
