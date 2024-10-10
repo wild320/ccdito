3e6f253dc6dc903e8a1c75a31d9c6d5591a639f4
dito <info@magicomundo.co>
2024-10-10 11:56:52 -0500

    add

--- src/app/shared/services/negocio.service.ts
+++ src/app/shared/services/negocio.service.ts
@@ -11,33 +11,23 @@ import { firstValueFrom } from 'rxjs';
   providedIn: 'root'
 })
 export class NegocioService {
-
-  httpClient = inject(HttpClient);
-
-  headers: HttpHeaders = new HttpHeaders();
-
-  UrlJsonConfguracion: string;
-
-  UrlServicioCarroCompras: string;
-
-  configuracion: any = {};
-
-  constructor() {
-    this.UrlJsonConfguracion = Cconfiguracion.urlAssetsConfiguracion + Cconfiguracion.JsonConfiguracion;
-  }
-
+  private httpClient = inject(HttpClient);
+  public configuracion: any;
+  private UrlJsonConfguracion: string = Cconfiguracion.urlAssetsConfiguracion + Cconfiguracion.JsonConfiguracion;
+  public UrlServicioCarroCompras: string = '';
 
   async loadSettingsFromServer(): Promise<any> {
-    this.headers = this.headers.append('Access-Control-Allow-Origin', '*');
-
     const options = {
-      headers: this.headers
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+      })
     };
 
     try {
-      const config: any = await firstValueFrom(this.httpClient.get(this.UrlJsonConfguracion, options));
+      const config = await firstValueFrom(this.httpClient.get<any>(this.UrlJsonConfguracion, options));
       this.configuracion = config;
       this.UrlServicioCarroCompras = `${this.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioConfiguracionCC}`;
+
     } catch (err) {
       console.error('Error leyendo json de configuracion:', err);
     }
