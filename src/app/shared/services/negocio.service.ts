a8f173378ae1f9ce6a3da5ae655ecf59da151316
dito <info@magicomundo.co>
2024-10-15 14:39:19 -0500

    config

--- src/app/shared/services/negocio.service.ts
+++ src/app/shared/services/negocio.service.ts
@@ -5,51 +5,32 @@ import { isPlatformBrowser } from '@angular/common';
 // Contantes
 import { Cconfiguracion } from '../../../data/contantes/cConfiguracion';
 import { CServicios } from 'src/data/contantes/cServicios';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NegocioService {
-
-  httpClient = inject(HttpClient);
-
-  headers: HttpHeaders = new HttpHeaders();
-
-  UrlJsonConfguracion: string;
-  
-  UrlServicioCarroCompras: string;
-
-  configuracion: any = {};
-
-  constructor(
-    @Inject(PLATFORM_ID) private platformId: Object) {
-    if (isPlatformBrowser(this.platformId)) {
-      const base = document.getElementsByTagName('base')[0].href;
-      this.UrlJsonConfguracion = base + Cconfiguracion.urlAssetsConfiguracion + Cconfiguracion.JsonConfiguracion;
-     
-    } 
-  }
+  private httpClient = inject(HttpClient);
+  public configuracion: any;
+  private UrlJsonConfguracion: string = "https://copiacarro--magico-mundo.us-central1.hosted.app/" + Cconfiguracion.urlAssetsConfiguracion + Cconfiguracion.JsonConfiguracion;
+  public UrlServicioCarroCompras: string = '';
 
   async loadSettingsFromServer(): Promise<any> {
-    if (isPlatformBrowser(this.platformId)) {
-      this.headers = this.headers.append('Access-Control-Allow-Origin', '*');
+    const options = {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*',
+      })
+    };
 
-      const options = {
-        headers: this.headers
-      };
+    try {
+      const config = await firstValueFrom(this.httpClient.get<any>(this.UrlJsonConfguracion, options));
+      this.configuracion = config;
+      this.UrlServicioCarroCompras = `${this.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioConfiguracionCC}`;
 
-      return this.httpClient.get(this.UrlJsonConfguracion, options)
-        .toPromise()
-        .then((config: any) => {
-          this.configuracion = config;
-          this.UrlServicioCarroCompras = `${this.configuracion.UrlServicioCarroCompras}${CServicios.ApiCarroCompras}${CServicios.ServicioConfiguracionCC}`;
-        })
-        .catch((err: any) => {
-          console.error('Error leyendo json de configuracion:' + err);
-        });
-    } else {
-      // Handle server-side case if necessary
-      return Promise.resolve(); // Or handle it differently
+    } catch (err) {
+      console.error('Error leyendo json de configuracion:', err);
     }
   }
+
 }
