3e3b3c3f293c182b6016fd8e430b040a2f4b575a
dito <info@magicomundo.co>
2024-10-21 15:09:21 -0500

    metatags en el resolver

--- src/app/modules/shop/resolvers/product.resolver.ts
+++ src/app/modules/shop/resolvers/product.resolver.ts
@@ -1,30 +1,56 @@
 import { inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import type { ResolveFn } from '@angular/router';
 import { ArticulosService } from 'src/app/shared/services/articulos.service';
+import { NegocioService } from 'src/app/shared/services/negocio.service';
 import { Item } from 'src/data/modelos/articulos/Items';
 
 export const productResolver: ResolveFn<Item> = async (route, state) => {
   const articulossvc = inject(ArticulosService);
-  const productSlug = route.paramMap.get('productSlug'); // Obtener el parámetro de la URL
+  const negocioSvc = inject(NegocioService);
+  const metaTagService = inject(Meta);
+  const titleService = inject(Title);
 
-  // Intentar obtener el producto desde el servicio (si ya está cargado en memoria)
+  const productSlug = route.paramMap.get('productSlug');
   let cachedProduct = articulossvc.getArticuloDetalle()?.item;
 
   if (!cachedProduct && productSlug) {
     try {
-      // Hacer la petición HTTP para obtener el detalle del producto si no está cacheado
       await articulossvc.SetSeleccionarArticuloDetalle(Number(productSlug), true);
       cachedProduct = articulossvc.getArticuloDetalle()?.item;
 
-      // Verificar si el producto fue correctamente recuperado
       if (!cachedProduct) {
         throw new Error(`Producto no encontrado para el slug ${productSlug}`);
       }
+
+      // Aquí puedes configurar los meta tags
+      const negocio = negocioSvc.configuracion;
+      const { name, caracteristicas, brand, images, price, rating, inventario, urlAmigable, id } = cachedProduct;
+
+      titleService.setTitle(`${negocio.NombreCliente} | ${name}`);
+      const description = caracteristicas || 'Compra este producto de alta calidad al mejor precio.';
+      const title = `${name} - ${brand?.['name'] || 'Marca Desconocida'} - Disponible en nuestra tienda`;
+      const keywords = `${name}, ${brand?.['name']}, precio, comprar, ${rating} estrellas, ${inventario} en stock, ${price}`;
+      const imageUrl = images?.length ? images[0] : `${negocio.baseUrl}assets/configuracion/LOGO2.png`;
+
+      metaTagService.addTags([
+        { name: 'description', content: description },
+        { name: 'title', content: title },
+        { name: 'keywords', content: keywords },
+        { property: 'og:title', content: title },
+        { property: 'og:description', content: description },
+        { property: 'og:image', content: imageUrl },
+        { property: 'og:url', content: `${negocio.baseUrl}/shop/products/${id}/${urlAmigable}` },
+        { name: 'twitter:title', content: title },
+        { name: 'twitter:description', content: description },
+        { name: 'twitter:image', content: imageUrl }
+      ]);
+
     } catch (error) {
       console.error('Error fetching product details', error);
-      return null; // Manejar el error devolviendo null
+      return null;
     }
   }
 
-  return cachedProduct; // Devolver el producto cacheado o recuperado
+  return cachedProduct;
 };
